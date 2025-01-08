0cabfca30cebf0912e2d377aa1159c4f45db81c8
Mohamed Sayed <ms7237322@gmail.com>
2025-01-08 03:25:33 +0200

    sidebar

---
+++ src/components/ui/layout.tsx
@@ -0,0 +1,17 @@
+// import { AppSidebar } from "@/components/ui/app-sidebar"
+// import { SidebarProvider } from "@/components/ui/sidebar"
+
+// export default function DashboardLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <SidebarProvider>
+//       <div className="flex h-screen overflow-hidden">
+//         <AppSidebar />
+//         <main className="flex-1 overflow-y-auto">{children}</main>
+//       </div>
+//     </SidebarProvider>
+//   )
+// }
